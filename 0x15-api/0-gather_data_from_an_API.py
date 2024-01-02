46914fb9da3a93db2b27a9e6ebf25265edb1856d
root <sandramabonga303@gmail.com>
2024-01-02 13:45:55 -0800

    Rest API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -16,30 +16,32 @@ if len(sys.argv) != 2:
 employee_id = int(sys.argv[1])
 
 # API endpoint URL
-api_url = "https://jsonplaceholder.typicode.com"
+api_url = "https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
 response = requests.get(api_url)
 
 # Check if the request was successful (HTTP status code 200)
 if response.status_code == 200:
     # Parse the JSON response
     todo_list = response.json()
+    if todo_list:
+        # Extract employee name
+        name = todo_list[0]['username']
 
-    # Extract employee name
-    name = todo_list[0]['username']
+        # Count the number of completed tasks
+        done_tasks = [task for task in todo_list if task['completed']]
+        num_tasks = len(done_tasks)
 
-    # Count the number of completed tasks
-    done_tasks = [task for task in todo_list if task['completed']]
-    done_tasks = len(done_tasks)
+        # Total number of tasks
+        total = len(todo_list)
 
-    # Total number of tasks
-    total_tasks = len(todo_list)
+        # Display employee information and TODO list progress
+        print(f"Employee {name} is done with tasks ({num_tasks}/{total}):")
 
-    # Display employee information and TODO list progress
-    print(f"Employee {name} is done with tasks ({done_tasks}/{total_tasks}):")
-
-    # Display titles of completed tasks
-    for task in done_tasks:
-        print(f"\t{task['title']}")
+        # Display titles of completed tasks
+        for task in done_tasks:
+            print(f"\t{task['title']}")
+    else:
+        print(f"No TODO list found for employee ID {employee_id}")
 
 else:
     # Display an error message if the request was not successful
