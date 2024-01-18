ccb8f5b946669af6809ef3959516e761f7cc9551
sandra113 <sandramabonga303@gmail.com>
2024-01-18 02:07:14 -0800

    number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module retrieves the number of subscribers from a given subredit
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    returns the number of subscribers from a given subredit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {'User-Agent': 'Sandra'}
+
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
+        return 0
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
