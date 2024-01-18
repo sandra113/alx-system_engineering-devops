e336cd800f746d9ba5bdb235d7dcf85d2f71ef3d
sandra113 <sandramabonga303@gmail.com>
2024-01-18 02:13:51 -0800

    number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,8 +2,9 @@
 """
 This module retrieves the number of subscribers from a given subredit
 """
-
 import requests
+from sys import argv
+
 
 def number_of_subscribers(subreddit):
     """
@@ -17,6 +18,7 @@ def number_of_subscribers(subreddit):
         return url.get('data').get('subscribers')
     except Exception:
         return 0
+
 
 if __name__ == "__main__":
     number_of_subscribers(argv[1])
