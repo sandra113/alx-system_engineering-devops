4587df4fd81da3c4307c8153a8e6070b534d29fc
sandra113 <sandramabonga303@gmail.com>
2024-01-18 02:53:01 -0800

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module queries the Reddit API
+and prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+import requests
+
+def top_ten(subreddit):
+    """
+    Reddit API endpoint for getting hot posts in a subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Sandra'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+
+
+        for posts in posts:
+            title = post['data']['title']
+            print(title)
+    elif response.status_code == 404:
+        print(f"Subreddit '{subreddit}' not found.")
+    else:
+        print(f"Error: {response.status_code}")
